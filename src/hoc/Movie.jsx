e6d17eda7c06ecee382f7084f0c27f9edad78798
Seyed Amir Mehrizi <s.a.mehrizi1989@gmail.com>
2022-09-12 13:05:13 +0430

    implement HOC

---
+++ src/hoc/Movie.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import withTooltip from './withTooltip'
+
+ class Movie extends Component {
+  render() {
+    return (
+      <div>
+        Movie
+        {this.props.showTooltip && <div>Some Tooltip</div>}
+        </div>
+    )
+  }
+}
+
+export default withTooltip(Movie);
+
+
